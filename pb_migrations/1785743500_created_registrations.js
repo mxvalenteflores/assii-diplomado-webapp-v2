ec93d4edca186a4ba12ba56e644462cd8992c3bc
mxvalenteflores <vadeflo@gmail.com>
2026-08-03 10:11:26 +0200

    fix: use plain objects in migration instead of SchemaField constructor

--- pb_migrations/1785743500_created_registrations.js
+++ pb_migrations/1785743500_created_registrations.js
@@ -3,62 +3,76 @@ migrate((app) => {
   const collection = new Collection({
     "createRule": "",
     "deleteRule": "@request.auth.id != \"\"",
-    "listRule": "@request.auth.id != \"\"",
-    "viewRule": "@request.auth.id != \"\"",
-    "updateRule": "@request.auth.id != \"\"",
+    "fields": [
+      {
+        "autogeneratePattern": "[a-z0-9]{15}",
+        "hidden": false,
+        "id": "text3208210256",
+        "max": 15,
+        "min": 15,
+        "name": "id",
+        "pattern": "^[a-z0-9]+$",
+        "presentable": false,
+        "primaryKey": true,
+        "required": true,
+        "system": true,
+        "type": "text"
+      },
+      {
+        "autogeneratePattern": "",
+        "hidden": false,
+        "id": "text_reg_sid",
+        "max": 0,
+        "min": 0,
+        "name": "studentId",
+        "pattern": "",
+        "presentable": false,
+        "primaryKey": false,
+        "required": false,
+        "system": false,
+        "type": "text"
+      },
+      {
+        "autogeneratePattern": "",
+        "hidden": false,
+        "id": "text_reg_fid",
+        "max": 0,
+        "min": 0,
+        "name": "formId",
+        "pattern": "",
+        "presentable": false,
+        "primaryKey": false,
+        "required": false,
+        "system": false,
+        "type": "text"
+      },
+      {
+        "autogeneratePattern": "",
+        "hidden": false,
+        "id": "text_reg_data",
+        "max": 0,
+        "min": 0,
+        "name": "data",
+        "pattern": "",
+        "presentable": false,
+        "primaryKey": false,
+        "required": false,
+        "system": false,
+        "type": "text"
+      }
+    ],
     "id": "pbc_3135925398",
+    "indexes": [],
+    "listRule": "@request.auth.id != \"\"",
     "name": "registrations",
     "system": false,
     "type": "base",
-  })
+    "updateRule": "@request.auth.id != \"\"",
+    "viewRule": "@request.auth.id != \"\""
+  });
 
-  collection.fields.add(
-    new SchemaField({
-      "autogeneratePattern": "",
-      "hidden": false,
-      "id": "text724990059",
-      "max": 0,
-      "min": 0,
-      "name": "studentId",
-      "pattern": "",
-      "presentable": false,
-      "primaryKey": false,
-      "required": false,
-      "system": false,
-      "type": "text"
-    }),
-    new SchemaField({
-      "autogeneratePattern": "",
-      "hidden": false,
-      "id": "text724990060",
-      "max": 0,
-      "min": 0,
-      "name": "formId",
-      "pattern": "",
-      "presentable": false,
-      "primaryKey": false,
-      "required": false,
-      "system": false,
-      "type": "text"
-    }),
-    new SchemaField({
-      "autogeneratePattern": "",
-      "hidden": false,
-      "id": "text724990061",
-      "max": 0,
-      "min": 0,
-      "name": "data",
-      "pattern": "",
-      "presentable": false,
-      "primaryKey": false,
-      "required": false,
-      "system": false,
-      "type": "text"
-    })
-  )
-
-  return app.save(collection)
+  return app.save(collection);
 }, (app) => {
-  const collection = app.findCollectionByNameOrId("pbc_3135925398")
-  return app.delete(collection)
+  const collection = app.findCollectionByNameOrId("pbc_3135925398");
+  return app.delete(collection);
 })
